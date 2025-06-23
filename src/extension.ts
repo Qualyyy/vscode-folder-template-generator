d61af3db2e9564c161c43ecaaf28022ddad44fd7
Qualyyy <Jarne.QUALY@student.howest.be>
2025-06-23 20:28:47 +0200

    prompt user to select a template

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,18 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "folder-template-generator" is now active!');
 
 	let disposable = vscode.commands.registerCommand('folder-template-generator.generateTemplate', async () => {
+		const config = vscode.workspace.getConfiguration('folderTemplateGenerator');
+		const structures = config.get<any[]>('structures') || [];
+
+		if (structures.length === 0) {
+			vscode.window.showErrorMessage('Please create a template first.');
+			return;
+		}
+
+		const structureNames = structures.map(structure => structure.name);
+		const selectedStructureName = await vscode.window.showQuickPick(structureNames, { placeHolder: 'Select a structure' });
+		const selectedStructure = structures.find(s => s.name === selectedStructureName);
+		vscode.window.showInformationMessage(selectedStructure.name);
 
 	});
 
