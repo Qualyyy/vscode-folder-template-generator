c566d3f7f2824fa1faba19502a43206d340f8c6d
Qualyyy <Jarne.QUALY@student.howest.be>
2025-06-24 13:12:25 +0200

    Add optional lines in templates

--- src/extension.ts
+++ src/extension.ts
@@ -35,12 +35,14 @@ export function activate(context: vscode.ExtensionContext) {
 		const selectedStructureName = await vscode.window.showQuickPick(structureNames, { placeHolder: 'Select a structure' });
 		const selectedStructure = structures.find(s => s.name === selectedStructureName);
 
+		// Check variables
 		const variables: { [key: string]: string } = {};
 		for (const variable of selectedStructure.variables) {
 			const value = await vscode.window.showInputBox({ prompt: variable }) || '';
 			variables[variable] = value;
 		}
 
+		// Check optional values
 		const optionals: { [key: string]: boolean } = {};
 		for (const item of selectedStructure.structure) {
 			if (item.optional && !(item.optional in optionals)) {
@@ -86,7 +88,26 @@ export function activate(context: vscode.ExtensionContext) {
 			// Make content match the template
 			const contentParts = fileTemplates[fileTemplate];
 			if (contentParts) {
-				fileContent = contentParts.join('\n');
+				let filteredParts: string[] = [];
+
+				for (let part of contentParts) {
+					const match = part.match(/\[([a-zA-Z0-9_]+)\]/);
+					if (match) {
+						const marker = match[1];
+						if (marker in optionals) {
+							// Skip if false
+							if (!optionals[marker]) {
+								continue;
+							}
+							// Remove marker if true
+							part = part.replace(match[0], '');
+						}
+					}
+
+					filteredParts.push(part);
+				}
+
+				fileContent = filteredParts.join('\n');
 
 				// Replace variables with correct value
 				for (const key in variables) {
