a208471aefcc3de51f946447fc10602fea202836
Qualyyy <Jarne.QUALY@student.howest.be>
2026-01-25 16:21:32 +0100

    Apply uppercase

--- src/utils/caseUtils.ts
+++ src/utils/caseUtils.ts
@@ -2,6 +2,8 @@ export function applyCase(input: string, caseType: string) {
     switch (caseType.toLowerCase()) {
         case 'lowercase':
             return input.toLowerCase();
+        case 'uppercase':
+            return input.toUpperCase();
         default:
             return input;
     }
